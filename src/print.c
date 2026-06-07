acaa31ee9f9b9efde47e65a12cbab7b71454d725
abdurehman4 <arehmanimran4@gmail.com>
2026-06-07 18:54:51 +0500

    Fixed printing on smaller terminal widths

--- src/print.c
+++ src/print.c
@@ -34,10 +34,16 @@ int calcColWidth(int percentage) {
 
 void printCompleted(packageInfo *package) {
   printf("\x1B[K"); // Clear line
-  printf(GREEN "%-*s"
-               "%s\n",
-         calcColWidth(60), package->packageName,
-         RED "::" WHITE " Download Completed!!");
+  if (getTerminalWidth() >= 110)
+    printf(GREEN "%-*s"
+                 "%s\n",
+           calcColWidth(60), package->packageName,
+           RED "::" WHITE " Download Completed!!");
+  else
+    printf(GREEN "%-*s"
+                 "%s\n",
+           calcColWidth(50), package->packageName,
+           RED "::" WHITE " Download Completed!!");
 }
 
 void printDownloadInfo(packageInfo *package) {
@@ -45,17 +51,20 @@ void printDownloadInfo(packageInfo *package) {
   if (package->packageName != NULL && package->speed != NULL &&
       package->downloaded != NULL && package->totalSize != NULL) {
 
-    if (getTerminalWidth() >= 110) {
-      printf(GREEN "%-*s" WHITE, calcColWidth(43), package->packageName);
-      printf("%*s ", calcColWidth(8) - 1, package->downloaded);
-      printf("%*s/s ", calcColWidth(10) - 3, package->speed);
+    if (getTerminalWidth() >= 110) { // For Normal Terminal Widths
+      printf(GREEN "%-*.*s" WHITE "%*.*s%*.*s/s ", calcColWidth(43),
+             calcColWidth(43) - 2, package->packageName, calcColWidth(8),
+             calcColWidth(8), package->downloaded, calcColWidth(10) - 3,
+             calcColWidth(10) - 4, package->speed);
       printf(GREEN);
       printProgress(package->progress, calcColWidth(39) - 4);
       printf(WHITE "%3d%%\n", package->progress);
-    } else {
-      printf(GREEN "%-*s" WHITE, calcColWidth(30), package->packageName);
-      printf("%*s ", calcColWidth(14) - 1, package->downloaded);
-      printf("%*s/s ", calcColWidth(17) - 3, package->speed);
+
+    } else { // For Smaller Terminal Widths
+      printf(GREEN "%-*.*s" WHITE "%*.*s%*.*s/s ", calcColWidth(30),
+             calcColWidth(30) - 2, package->packageName, calcColWidth(14),
+             calcColWidth(14), package->downloaded, calcColWidth(17) - 3,
+             calcColWidth(17) - 4, package->speed);
       printf(GREEN);
       printProgress(package->progress, calcColWidth(39) - 4);
       printf(WHITE "%3d%%\n", package->progress);
