343e9901cc46912d5684ee75fa9c917824688eab
abdurehman4 <arehmanimran4@gmail.com>
2026-06-24 19:58:22 +0500

    Change: exit(1) to return NULL

--- src/urls.c
+++ src/urls.c
@@ -2,12 +2,12 @@
 #include "colors.h"
 #include "str.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 packageURL getPackageURL(char *package) {
   FILE *process;
+  char *returnUrls = NULL;
   String *urls; // getOutput allocates memory itself
   String *command;
 
@@ -16,21 +16,22 @@ packageURL getPackageURL(char *package) {
     stringAppend(&command, package);
   } else {
     printf(RED "Error: " WHITE "Package Name is NULL!!");
-    exit(1);
+    return NULL;
   }
 
   if ((process = popen(command->str, "r")) == NULL) {
     printf("Failed to get URL");
-    exit(1);
+    return NULL;
   }
 
   if ((urls = getOutput(process)) == NULL) {
     printf(RED "Error:" WHITE " No URL found for the package: %s\n", package);
-    exit(1);
+    pclose(process);
+    return NULL;
   }
   pclose(process);
 
-  char *returnUrls = strdup(urls->str);
+  returnUrls = strdup(urls->str);
   freeString(command);
   freeString(urls);
   returnUrls[strcspn(returnUrls, "\n")] = '\0';
