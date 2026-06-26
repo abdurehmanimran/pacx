a2f64c6d5803821a6efd924ead25777e65b89cdb
abdurehman4 <arehmanimran4@gmail.com>
2026-06-26 10:22:47 +0500

    Improvement: better progress bar

--- src/progress.c
+++ src/progress.c
@@ -1,11 +1,21 @@
 #include "progress.h"
+#include "colors.h"
 #include <stdio.h>
 
 void printProgress(double progress, int width) {
   int dashes = (progress / 100) * width;
 
+  if (progress == 0)
+    printf(BLACK);
+
   for (int i = 0; i < dashes; i++)
-    printf("%s", "\u2501");
+    printf("%s", BAR);
+
+  if (progress > 0 && progress < 100) {
+    printf(BLUE "%s" BLACK, SPLIT_BAR);
+    dashes++;
+  }
+
   for (int i = 0; i < width - dashes; i++)
-    printf("%s", "\u254D");
+    printf("%s", BAR);
 }
--- src/progress.h
+++ src/progress.h
@@ -1,6 +1,9 @@
 #ifndef PROGRESS_H
 #define PROGRESS_H
 
+#define BAR "\u2501"
+#define SPLIT_BAR "\u254D"
+
 void printProgress(double progress, int width);
 
 #endif // !PROGRESS_H
