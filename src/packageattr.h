f5c87b73881114c6f07d7c70f0e73eda8c6615a4
Abdur Rehman Imran <arehmanimran4@gmail.com>
2026-06-30 20:29:41 +0500

    Feat: added func to get path of mirrorlist file

--- src/packageattr.h
+++ src/packageattr.h
@@ -1,6 +1,8 @@
 #ifndef PACKAGE_ATTR_H
 #define PACKAGE_ATTR_H
 
+#include "str.h"
+
 typedef struct {
   char *arch;
   char *repo;
@@ -10,5 +12,6 @@ typedef struct {
 packageAttr *initPackageAttr();
 void freePackageAttr(packageAttr *pkgAttr);
 packageAttr *getPackageAttrs(char *url);
+String *getMirrorListPath(packageAttr *attr);
 
 #endif
